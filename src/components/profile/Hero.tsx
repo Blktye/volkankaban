81d3411142cdb1ee794ac392aa950d99e301b7de
volkankaban <volkankaban@outlook.com>
2022-12-26 15:45:43 -0600

    update

--- src/components/profile/Hero.tsx
+++ src/components/profile/Hero.tsx
@@ -10,23 +10,12 @@ const Hero = () => {
   const { profile_name, username, avatar, bio, user_location, cover } = data;
   return (
     <>
-      <video
-        className="flex h-[300px] w-screen flex-col items-center object-cover"
-        src={cover}
-        muted="muted"
-        autoplay="autoplay"
-        loop="loop"
-        // playsinline=""
-      ></video>
+      <video className="flex h-[300px] w-screen flex-col items-center object-cover"
+        src={cover} muted="muted" autoplay="autoplay" loop="loop" playsinline="">
+      </video>
       <section className="absolute top-[250px] flex w-[300px] min-w-[200px] flex-col items-center justify-center ">
         <div className="h-24 w-24 overflow-hidden rounded-full border-2 object-contain">
-          <Image
-            src={avatar}
-            alt={profile_name}
-            width={200}
-            height={200}
-            priority
-          />
+          <Image src={avatar} alt={profile_name} width={200} height={200} priority/>
         </div>
         <h1 className="flex items-center justify-center text-xl font-bold">
           {profile_name}
